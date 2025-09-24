608877dc401c06db0feacbcf3f217dc9b065e64e
chodotdesign <159584698+chodotdesign@users.noreply.github.com>
2025-09-24 10:11:43 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-let serviceName = "";
-let APP_DOMAIN = "";
+let serviceName = "zip";
+let APP_DOMAIN = "company.web.id";
 
 let prxIP = "";
 let cachedPrxList = [];
